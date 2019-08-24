de0046b1673812a40eef7ab2be2a24c6b2c4708a
unknown <chandu986646@gmail.com>
2019-08-24 13:46:28 +0530

    practising..session

---
+++ hi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="enter number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   raptor_prompt_variable_zzyz ="enter number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   cout << " Add "+(a+b)+" Sub "+(a-b)+" Mul "+(a*b)+" Div "+(a/b) << endl;   cout << ((a*b)+b-a/a) << endl;
+   return 0;
+}
---
+++ hi.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace hi
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? b;
+         ?? a;
+      
+         raptor_prompt_variable_zzyz ="enter number";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         a= Double.Parse(Console.ReadLine());
+         raptor_prompt_variable_zzyz ="enter number";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         b= Double.Parse(Console.ReadLine());
+         Console.WriteLine(" Add "+(a+b)+" Sub "+(a-b)+" Mul "+(a*b)+" Div "+(a/b));
+         Console.WriteLine(((a*b)+b-a/a));
+      }
+   }
+}
